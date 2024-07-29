843a24a4c900e176a5344f3c1ffddcb5974b0542
Natnael Teklemariam <natnaelmverse1@gmail.com>
2024-07-29 14:08:15 +0300

    cors changed

--- app.ts
+++ app.ts
@@ -26,7 +26,7 @@ const limiter = rateLimit({
 // cors config
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: "https://addismelody-frontend.onrender.com", // Replace with your frontend URL
     credentials: true,
   })
 );
