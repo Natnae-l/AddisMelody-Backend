58b7652e3a9f20a20cb371de68d54b5a6b8b187f
Natnael Teklemariam <natnaelmverse1@gmail.com>
2024-07-30 18:43:35 +0300

    naming changed

--- controller/multer.ts
+++ controller/multer.ts
@@ -1,12 +1,13 @@
 import path from "path";
 import multer from "multer";
+import { StringDecoder } from "string_decoder";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, "../uploads/"));
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, String(Date.now()) + ".mp3");
   },
 });
 
