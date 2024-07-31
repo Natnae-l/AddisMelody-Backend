bdef3f6dee238172a283064ac45d3b004a9ddcc5
Natnael Teklemariam <natnaelmverse1@gmail.com>
2024-07-31 13:53:49 +0300

    domain changed

--- app.ts
+++ app.ts
@@ -28,7 +28,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://astp.andmtaskills.com",
-      "https://addismelody.netlify.app/",
+      "https://addismelody.netlify.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
